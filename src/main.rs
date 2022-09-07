6104dd87688807bbabd94a537498f90e65e804f9
wangzekai <wangzekai.parallel@bytedance.com>
2022-09-07 16:58:21 +0800

    axum and tracing hello_world for index.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, World.");
+use std::net::SocketAddr;
+use axum::response::Html;
+use axum::Router;
+use axum::routing::get;
+use tracing::Level;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+
+    // build application with route
+    let app = Router::new().route("/", get(index));
+
+    // run app with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await.unwrap();
 }
+
+async fn index() -> Html<&'static str> { Html(include_str!("../dist/index.html")) }
