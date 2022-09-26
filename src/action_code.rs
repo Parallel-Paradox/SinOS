1c663474d6bf2aad1e9d8f7f235a3963704299e5
wangzekai <wangzekai.parallel@bytedance.com>
2022-09-26 12:01:39 +0800

    RoomMap creation.

--- src/action_code.rs
+++ src/action_code.rs
@@ -4,22 +4,26 @@ use std::sync::Arc;
 use axum::routing::get;
 use axum::{Json, Router, Extension};
 use axum::response::IntoResponse;
+use mongodb::{Client, Database};
+use mongodb::options::{ClientOptions, ServerAddress};
+use crate::config::mongo_credential::action_code;
 pub use game_room::*;
 
 pub fn create_app() -> Router {
     let db = Arc::new(connect_mongodb());
+    let rp = Arc::new(RoomMap::new());
 
-    Router::new()
-        .route("/new_game_room", get(new_game_room))
-        .route("/test", get(test))
-        .layer(Extension(db))
+    let app = Router::new()
+        .route("/new_game_room", get(new_game_room));
+
+    if cfg!(debug_assertions) {
+        // Do some test for unstable API.
+        app.route("/experiment", get(experiment))
+    } else { app }
+        .layer(Extension(db)).layer(Extension(rp))
 }
 
-pub fn connect_mongodb() -> mongodb::Database {
-    use mongodb::Client;
-    use mongodb::options::{ClientOptions, ServerAddress};
-    use crate::config::mongo_credential::action_code;
-
+pub fn connect_mongodb() -> Database {
     let mut client_option = ClientOptions::default();
     client_option.app_name = Some("ActionCode".into());
     client_option.hosts = vec![ServerAddress::Tcp {
@@ -31,13 +35,21 @@ pub fn connect_mongodb() -> mongodb::Database {
     Client::with_options(client_option).unwrap().database("action_code")
 }
 
+/// Create a new [`GameRoom`], return the [`RoomID`].
 async fn new_game_room() -> impl IntoResponse {
+
+    // TODO Save this game room into redis.
     let game_room = GameRoom::default();
-    println!("{:?}", game_room);
 
     Json(game_room)
 }
 
-async fn test(Extension(db): Extension<Arc<mongodb::Database>>) -> impl IntoResponse {
+/// Do some test for unstable API.
+#[cfg(debug_assertions)]
+async fn experiment(
+    Extension(db): Extension<Arc<Database>>,
+    Extension(rp): Extension<Arc<RoomMap>>,
+) -> impl IntoResponse
+{
     Json(get_random_words(db, 25).await.unwrap())
 }
--- src/action_code/game_room.rs
+++ src/action_code/game_room.rs
@@ -1,22 +1,50 @@
-use std::sync::Arc;
+use std::{sync::Arc, collections::HashMap, fmt::Display};
 
 use futures::StreamExt;
 use mongodb::{Database, bson::{doc, Document, from_bson}};
 use nanoid::nanoid;
+use parking_lot::RwLock;
 use serde::{Serialize, Deserialize};
 use crate::config::{NUM_ALPHABET, Result};
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct RoomID(String);
+/// Hold [`RwLock`] of the whole map and each entry in this map. Access the write lock of the whole
+/// map only when create or delete a game room.
+#[derive(Debug)]
+pub struct RoomMap(RwLock<HashMap<RoomID, RwLock<GameRoom>>>);
+
+impl RoomMap {
+    pub fn new() -> Self { Self::default() }
+}
+
+impl Default for RoomMap {
+    fn default() -> Self { Self(RwLock::new(HashMap::new())) }
+}
+
+
+/// Access [`RoomMap`] by this. Copy trait implied.
+#[derive(Debug, Serialize, Deserialize, Eq, Hash, Copy, Clone)]
+pub struct RoomID(usize);
 
 impl RoomID {
-    pub fn get(&self) -> &str { &self.0 }
+    pub fn new() -> Self { Self::default() }
 }
 
 impl Default for RoomID {
-    fn default() -> Self { Self(nanoid!(7, &NUM_ALPHABET)) }
+    fn default() -> Self { Self(nanoid!(7, &NUM_ALPHABET).parse().unwrap()) }
 }
 
+impl Display for RoomID {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "RoomID({:07})", self.0)
+    }
+}
+
+impl PartialEq for RoomID {
+    fn eq(&self, other: &Self) -> bool { self.0 == other.0 }
+}
+
+
+/// Save the context of a Game room.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GameRoom {
     pub room_id: RoomID,
@@ -25,9 +53,10 @@ pub struct GameRoom {
 
 impl Default for GameRoom {
     fn default() -> Self {
-        Self { room_id: RoomID::default(), word_list: None, }
+        Self { room_id: RoomID::new(), word_list: None, }
     }
 }
+
 
 /// Get 25 random words from the given mongo database.
 pub async fn get_random_words(db: Arc<Database>, size: i32) -> Result<Vec<String>> {
